b51029e44abaee2f2556d09aaff21f9116d62c92
hagarabdelmotalb <114605750+hagarabdelmotalb@users.noreply.github.com>
2025-10-29 00:28:35 +0300

    PictureUrl

--- Core/Services/MappingProfiles/PictureUrlResolver.cs
+++ Core/Services/MappingProfiles/PictureUrlResolver.cs
@@ -1,15 +1,17 @@
 ﻿using AutoMapper;
 using Domain.Entities.ProductModule;
+using Microsoft.Extensions.Configuration;
 using Shared.Dtos;
+using System.Buffers.Text;
 
 namespace Services.MappingProfiles
 {
-    public class PictureUrlResolver : IValueResolver<Product, ProductResultDto, string>
+    public class PictureUrlResolver(IConfiguration _configuration) : IValueResolver<Product, ProductResultDto, string>
     {
         public string Resolve(Product source, ProductResultDto destination, string destMember, ResolutionContext context)
         {
             if(string.IsNullOrEmpty(source.PictureUrl)) return string.Empty;
-            return $"{source.PictureUrl}";
+            return $"{_configuration.GetSection("URLS")["BaseUrl"]}{source.PictureUrl}";
         }
     }
 }
