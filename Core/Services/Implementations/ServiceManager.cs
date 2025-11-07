20ca3b14e96f2df63863fa2459c0d15e43617e6a
hagarabdelmotalb <114605750+hagarabdelmotalb@users.noreply.github.com>
2025-11-07 03:00:22 +0200

    Authentication controller

--- Core/Services/Implementations/ServiceManager.cs
+++ Core/Services/Implementations/ServiceManager.cs
@@ -1,14 +1,21 @@
 ﻿using AutoMapper;
 using Domain.Contracts;
+using Domain.Entities.IdentityModule;
+using Microsoft.AspNetCore.Identity;
 using Services.Abstraction.Contracts;
 
 namespace Services.Implementations
 {
-    public class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper, IBasketRepository _basketRepository) : IServiceManager
+    public class ServiceManager(IUnitOfWork _unitOfWork,
+        IMapper _mapper, IBasketRepository _basketRepository,
+        UserManager<User> _userManager) : IServiceManager
     {
         private readonly Lazy<IProductService> _productService = new Lazy<IProductService>(() => new ProductSerivce(_unitOfWork, _mapper));
         private readonly Lazy<IBasketService> _basketService = new Lazy<IBasketService>(() => new BasketService(_basketRepository, _mapper));
+        private readonly Lazy<IAuthenticationService> _authService = new Lazy<IAuthenticationService>(() => new AuthenticationService(_userManager));
+
         public IProductService ProductService => _productService.Value;
         public IBasketService BasketService => _basketService.Value;
+        public IAuthenticationService AuthenticationService => _authService.Value;
     }
 }
---
+++ Infrastructure/Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstraction.Contracts;
+using Shared.Dtos.IdentityModule;
+
+namespace Presentation.Controllers
+{
+    public class AuthenticationController(IServiceManager _serviceManager) : ApiController
+    {
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserResultDto>> ResultAsync(RegisterDto registerDto)
+            => Ok(await _serviceManager.AuthenticationService.RegisterAsync(registerDto));
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserResultDto>> LoginAsync(LoginDto loginDto)
+            => Ok(await _serviceManager.AuthenticationService.LoginAsync(loginDto));
+
+    }
+}
